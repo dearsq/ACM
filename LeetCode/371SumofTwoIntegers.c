84f3fb8b3c3eb4a476c3c93c9cb498b51a3ed52f
Younix <foreveryounix@gmail.com>
2016-09-19 09:40:01 +0800

    LeetCode 371SumofTwoIntegers

---
+++ LeetCode/371SumofTwoIntegers.c
@@ -0,0 +1,40 @@
+/*************************************************************************
+    > File Name: 371SumofTwoIntegers.c
+    > Author: Younix
+    > Mail: foreveryounix@gmail.com 
+    > Created Time: 2016年09月19日 星期一 08时52分52秒
+
+	371. Sum of Two Integers
+	Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+	Example:
+	Given a = 1 and b = 2, return 3.
+ ************************************************************************/
+
+#include <stdio.h>
+
+
+int
+main ()
+{
+  int getSum (int a, int b)
+  {
+
+    int Carry = 0;
+
+    int a_fun = a;
+
+    int b_fun = b;
+
+    while (b_fun)
+      {
+		Carry = (a_fun & b_fun) << 1;
+
+		a_fun = a_fun ^ b_fun;
+
+		b_fun = Carry;
+      }
+
+    return a_fun;
+
+  }
+}
